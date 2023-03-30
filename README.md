210a7cab5239e4aa55097c5346adc71d3de46dc9
MaiaPelletier <maia.pelletier15@gmail.com>
2023-03-30 10:02:00 -0400

    Final submission

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# digitall-dataviz-challenge
+# DigitALL Data Visualization Challenge  
+
+My entry for the IWD data viz challenge looks at the gap between income and education in Canadian women and men who are both visible minorities and not visibile minorities. I chose to include the census "visible minority" dimension in my analysis because often in the discussion of the wage gap, racialized women are left out of the discussion. In terms of income, on average these women are even more under-paid than women who do not belong to a racialized group.  
+
+I acknowledge that my visualization leaves a lot of context out of the picure - my goal was to paint a broad picture of education and income across gender and demographic dimensions. There is a lot more nuance that I didn't have the time to cover! The biggest thing to note here is that the "visibile minority" definition excludes Indigenous popuations, which means they are included in the non-visible minority population.  
+
+And on that note... this is overall a rough visualization. I would love to improve/polish it given more time :) 
+
+![image](income-education-gap.png)
--- income-education-gap.R
+++ income-education-gap.R
@@ -1,153 +1,252 @@
-# dimensions being used for income
-# income source = market income
-# income statistic = average income
-
-# dimensions being used for education
-# highest degree = post secondary
-# statistic = % distribution
-
-# IDEAS
-# Map colour to Men+/Women+
-# Map colour intensity to average income of province / % of degrees per province
-
-# VM isn't a good distinguisher because Indigenous is a separate category from 
-# "VM" and therefore "non-VM" is not necessarily a stand-in for "white" bc it
-# includes Indigenous populations
-
 # load libraries
 library(tidyverse)
 library(janitor)
-library(geofacet)
+library(ggtext)
+library(glue)
+library(extrafont)
+library(scales)
+
+
+# load data ---------------------------------------------------------------
+
 
 # renaming function
 rename_tables <- function(data) {
   data %>% 
-  clean_names() %>% 
-  rename_all(~ str_remove(.x, "_[0-9]+\\w*"))
+    clean_names() %>% 
+    rename_all(~ str_remove(.x, "_[0-9]+\\w*"))
 }
 
-# load data (downloaded from CODR tables)
+
+# load income data
+# downloaded from Table 98-10-0334-01 (source: 2021 Census)
 income <- 
   read_csv("income.csv") %>% 
   rename_tables() %>%
   select(
     geo,
-    visible_minority,
+    vm = visible_minority,
     gender,
     age,
     income = value
   )
 
+
+# load education data
+# downloaded from Table 98-10-0429-01 (source: 2021 Census)
 education <- 
   read_csv("education.csv")  %>%
   rename_tables() %>% 
   select(
     geo,
-    visible_minority,
+    vm = visible_minority,
     gender,
     age,
     education = value
   )
 
-# gender income gap -----
-gender_income_gap <-
-  income %>% 
-  filter(
-  visible_minority == "Total - Visible minority",
-  age == "25 to 64 years",
-  gender != "Total - Gender"
-  ) %>% 
-  pivot_wider(
-    names_from = gender,
-    values_from = income
-  ) %>% 
+
+# load population counts
+# downloaded from Table 98-10-0326-01 (source: 2021 Census)
+population <- 
+  read_csv("population.csv") %>% 
+  rename_tables() %>% 
+  select(
+    geo,
+    vm = visible_minority,
+    gender, age,
+    population = value
+  )
+
+
+# read csv with list of province abbreviations
+abbreviations <- read_csv("geo_abbreviations.csv")
+
+
+# data manipulation -------------------------------------------------------
+
+
+# join data and create intersection categories
+data <- 
+  left_join(income, education) %>% 
+  left_join(population) %>% 
+  left_join(abbreviations) %>% 
+  filter_at(2:3, ~ !str_detect(.x, "Total - ")) %>%   # filter out totals
   mutate(
-    diff = `Men+` - `Women+`,
-    total = `Men+` + `Women+`,
-    gender_income_gap = (diff / (total/2))*100
+    quad = case_when(
+      vm == "Total visible minority population" & gender == "Women+" ~ "VM Women",
+      vm == "Total visible minority population" & gender == "Men+" ~ "VM Men",
+      vm == "Not a visible minority" & gender == "Women+" ~ "non-VM Women",
+      vm == "Not a visible minority" & gender == "Men+" ~ "non-VM Men"
+    )
   ) %>% 
-  select(geo, gender_income_gap)
+  filter( 
+    geo != "Canada",
+    !code %in% c("YT", "NT", "NU")
+  )
 
-# minority income gap -----
-minority_income_gap <-
-  income %>% 
-  filter(
-    visible_minority != "Total - Visible minority",
-    age == "25 to 64 years",
-    gender == "Total - Gender"
-  ) %>% 
-  pivot_wider(
-    names_from = visible_minority,
-    values_from = income
-  ) %>% 
-  mutate(
-    diff = `Not a visible minority` - `Total visible minority population`,
-    total = `Not a visible minority` + `Total visible minority population`,
-    vm_income_gap = (diff / (total/2))*100
-  ) %>% 
-  select(geo, vm_income_gap)
 
-income_gap <- left_join(gender_income_gap, minority_income_gap)
+# calculate each demograhic % of total population
+pct_pop <-
+  data %>%
+  left_join(
+    population %>%
+      group_by(geo) %>%
+      summarise(total = sum(population), .groups = "drop")
+  ) %>%
+  mutate(pct_pop = (population/total)*100000)
 
-# gender education gap -----
-gender_education_gap <- 
-  education %>% 
-  filter(
-    visible_minority == "Total - Visible minority",
-    gender != "Total - Gender"
-  ) %>% 
-  pivot_wider(
-    names_from = gender,
-    values_from = education
-  ) %>% 
-  mutate(
-    gender_edu_gap = `Men+` - `Women+`
-  )  %>% 
-  select(geo, gender_edu_gap)
 
-# minority education gap -----
-minority_education_gap <- 
-  education %>% 
-  filter(
-    visible_minority != "Total - Visible minority",
-    age == "25 to 64 years",
-    gender == "Total - Gender"
-  ) %>% 
-  pivot_wider(
-    names_from = visible_minority,
-    values_from = education
-  ) %>% 
-  mutate(
-    vm_edu_gap = `Not a visible minority` - `Total visible minority population`
-  ) %>% 
-  select(geo, vm_edu_gap)
+# join population percents
+data <-
+  data %>%
+  left_join(pct_pop) %>% 
+  mutate(quad = factor(quad, levels = c("VM Women", 
+                                        "non-VM Women", 
+                                        "VM Men", 
+                                        "non-VM Men")))
 
-education_gap <- left_join(gender_education_gap, minority_education_gap)
 
-ggplot() +
-  geom_rect(
-    data = income_gap,
-    aes(xmin = 0, xmax = gender_income_gap, ymin = 0, ymax = vm_income_gap),
-    fill = "blue"
+# plot set-up -------------------------------------------------------------
+
+
+# line segments for grid squares
+line_segments <- data.frame(
+  xstart = c(rep(0, 3), seq(0, 100000, length.out = 3)),
+  xend = c(rep(100000, 3), seq(0, 100000, length.out = 3)),
+  ystart = c(seq(0, 100, 50), rep(0, 3)),
+  yend = c(seq(0, 100, 50), rep(100, 3))
+)
+
+
+# subtitle with HTML formatting
+subtitle <- "In 2021 <b>working-aged women</b> who were <b>visible minorities (VM)</b> had the <b>highest rates of post-secondary<br>education</b> in almost all Canadian provinces<sup>1</sup> and earned the <b>lowest annual salary</b> on average overall."
+
+
+# create footnotes with HTML formatting
+ftn_1 <- "<b> Working-aged</b> - Population aged 25 to 64"
+ftn_2 <- "<b> Women</b> - Includes women (and/or girls), as well as some non-binary persons."
+ftn_3 <- "<b> Visible minority (VM)</b> - A person who is a visible minority as defined by the <i>Employment Equity Act</i>. Consists mainly of the following groups: South Asian, Chinese, Black,  Filipino,<br>Arab, Latin American, Southeast Asian, West Asian, Korean and Japanese. Excludes Indigenous persons."
+ftn_4 <- "<b> Postsecondary education</b> - Postsecondary certificate, diploma or degree"
+ftn_5 <- " Territories are excluded          from analysis."
+source <- "<b>Source: </b> Statistics Canada, Census of Population, 2021. | <b>Author: </b> Maia Pelletier"
+
+
+# combine to create caption
+caption <- glue("<b>Definitions:</b><br>{ftn_1}<br>{ftn_2}<br>{ftn_3}<br>{ftn_4}<br><br><sup>1</sup>{ftn_5}<br><br><br>{source}")
+
+
+# create palette for plot
+pal <- c(
+  "#6daabd", # blue
+  "#bae79d", # green
+  "#f3cb04", # yellow
+  "#e28027", # red
+  "#FBF7EB"  # off-white
+)
+
+
+# load installed fonts
+extrafont::loadfonts()
+
+
+# ggplotting --------------------------------------------------------------
+
+
+data %>%
+  ggplot() +
+  geom_segment(
+    data = line_segments,
+    aes(x = xstart,
+        y = ystart,
+        xend = xend,
+        yend = yend),
+    color = "#E7D6C5"
   ) +
   geom_rect(
-    data = education_gap,
-    aes(xmin = 0, xmax = gender_edu_gap, ymin = 0, ymax = vm_edu_gap)
+    aes(
+      xmin = 0,
+      ymin = 0,
+      xmax = income,
+      ymax = education,
+      color = quad,
+      fill = quad
+    ),
+    alpha = 0.5
+  )+
+  geom_col(
+    data = 
+      data %>% 
+      mutate(quad = reorder(quad, pct_pop)),
+    aes(x = pct_pop, y = 98, fill = quad),
+    orientation = "y",
+    position = "stack",
+    width = 4,
+    alpha = 0.5
   ) +
-  facet_wrap(~ geo) +
-  xlim(c(-55, 55)) +
-  ylim(c(-55, 55))
+  labs(
+    x = "Average annual salary",
+    y = "% with postsecondary achievement",
+    title = str_to_upper("Over-educated & under-paid"),
+    subtitle = subtitle,
+    caption = caption
+  ) +
+  facet_wrap(~code, nrow = 2) +
+  scale_y_continuous(
+    limits = c(0, 110),
+    breaks = c(0, 50, 100),
+    labels = percent_format(scale = 1),
+    position = "left"
+  ) +
+  scale_x_continuous(
+    limits = c(0, 110000),
+    breaks = c(0, 50000, 100000),
+    labels = dollar_format(suffix = "k", scale = 0.001)
+  ) +
+  scale_fill_manual(
+    values = pal[c(4, 2, 3, 1)],
+    name = NULL
+  ) +
+  scale_color_manual(
+    values = pal[c(4, 2, 3, 1)],
+    name = NULL
+  ) +
+  theme_void() +
+  theme(
+    aspect.ratio = 1,
+    text = element_text(color = "#4D4519", 
+                        family = "Lato"),
+    plot.margin = margin(t = 20, b = 20, l = 20, r = 28),
+    plot.background = element_rect(fill = pal[5], 
+                                   color = NA),
+    panel.spacing = unit(10, "mm"),
+    plot.title = element_text(face = "bold", 
+                              size = 32, 
+                              hjust = 0.5, 
+                              margin = margin(b = 5), 
+                              family = "Comfortaa"),
+    plot.title.position = "plot",
+    plot.subtitle = element_markdown(size = 12, 
+                                     hjust = 0.5),
+    plot.caption = element_markdown(margin = margin(t = 20, b = -5, l = -50), 
+                                    size = 8, 
+                                    hjust = 0, 
+                                    lineheight = 1.4),
+    strip.text = element_text(face = "bold", 
+                              size = 10, 
+                              family = "Comfortaa"),
+    axis.text = element_text(size = 7, 
+                             margin = margin(l = 2, t = 2)),
+    axis.title.x = element_text(margin = margin(t = 10), 
+                                size = 12),
+    axis.title.y = element_text(angle = 90, 
+                                margin = margin(r = 15), 
+                                size = 12),
+    legend.position = "top",
+    legend.margin = margin(t = 20, b = 20),
+    legend.text = element_text(margin = margin(r = 50), size = 10)
+  )
 
-# # create geofacet grid
-# mygrid <- data.frame(
-#   row = c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2),
-#   col = c(1, 2, 4, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8),
-#   code = c("YT", "NT", "NU", "NL", "PE", "BC", "AB", "SK", "MB", 
-#            "ON", "QC", "NB", "NS"),
-#   name = c("Yukon", "Northwest Territories", "Nunavut", 
-#            "Newfoundland and Labrador", "Prince Edward Island", 
-#            "British Columbia", "Alberta", "Saskatchewan", "Manitoba", 
-#            "Ontario", "Quebec", "New Brunswick", "Nova Scotia"),
-#   stringsAsFactors = FALSE
-# )
+
+ggsave("income-education-gap.png", dpi = 300)
 
